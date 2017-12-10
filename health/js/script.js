47a70b49d27d62c287c3f482db563fff1c6234c1
Josh Li <oasislrjjrl@gmail.com>
2017-12-11 10:00:17 +1100

    Added styles for cards in resources and news listing pages.

--- health/js/script.js
+++ health/js/script.js
@@ -125,14 +125,4 @@
     }
   };
 
-  // Add classes and elements to external links.
-  Drupal.behaviors.selectorCardsClick = {
-    attach: function (context, settings) {
-      $('.selector-card').click(function(e) {
-        e.preventDefault();
-        location.href = $(this).find('a').attr('href');
-      });
-    }
-  };
-
 })(jQuery, Drupal, this, this.document);
