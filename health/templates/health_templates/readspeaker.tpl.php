6f584e3d4bae3678c499c63b2f0d5a97e066ccd5
Dylan Rolfe <dylan.rolfe@health.gov.au>
2018-01-10 11:59:22 +1100

    Remove VueJS implementation of RS as it was causing issues with the button disappearing.

--- health/templates/health_templates/readspeaker.tpl.php
+++ health/templates/health_templates/readspeaker.tpl.php
@@ -1,6 +1,8 @@
 <div id="readspeaker">
   <div id="readspeaker_button1" class="rs_skip rsbtn rs_preserve">
-    <a rel="nofollow" class="rsbtn_play js-ignoreext" accesskey="L" title="Listen to this page using ReadSpeaker" href="//app-oc.readspeaker.com/cgi-bin/rsent?customerid=5802&lang=en_au&readid=page-content&url=<?php print current_path()?>">
+    <a rel="nofollow" class="rsbtn_play js-ignoreext" accesskey="L"
+       title="Listen to this page using ReadSpeaker"
+       href="//app-oc.readspeaker.com/cgi-bin/rsent?customerid=5802&lang=en_au&readid=page-content&url=<?php print url(current_path(),['absolute' => TRUE,'query' => drupal_get_query_parameters()])?>">
       <span class="rsbtn_left rsimg rspart">
         <span class="rsbtn_text">
           <span>Listen</span>
