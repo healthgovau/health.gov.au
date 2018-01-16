b27252c0c339e69bce9b9b0b41aa289359e0796a
Josh Li <oasislrjjrl@gmail.com>
2018-01-16 15:36:58 +1100

    Preserved table of content for readspeaker.

--- health/templates/health_templates/toc.tpl.php
+++ health/templates/health_templates/toc.tpl.php
@@ -1,4 +1,4 @@
-<nav class="uikit-inpage-nav-links rs_skip">
+<nav class="uikit-inpage-nav-links rs_skip rs_preserve">
     <div class="uikit-inpage-nav-links__heading uikit-display-2">On this page</div>
     <div class="toc uikit-link-list"></div>
 </nav>
