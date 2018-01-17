ce26cc59a13ddf2ec128616ada25300a85b81f50
Danny <dannymarlow78@hotmail.com>
2018-01-17 13:44:11 +1100

    Added colours to task accelerator and added snippet before </body> for popup survey

--- health/templates/html.tpl.php
+++ health/templates/html.tpl.php
@@ -62,5 +62,8 @@
   <?php print $page; ?>
   <?php print $page_bottom; ?>
 
+  <!-- BEGIN Optimal Workshop Intercept Snippet -->
+  <div id='owInviteSnippet' style='position:fixed;right:20px;bottom:20px;width:280px;padding:20px;margin:0;border-radius:6px;background:#008998;color:#FAF5F5;text-align:left;z-index:2200000000;opacity:0;transition:opacity 500ms;-webkit-transition:opacity 500ms;display:none;'><div id='owInviteMessage' style='padding:0;margin:0 0 20px 0;font-size:16px;'>Got a spare 5 minutes to help us improve our website?</div><a id='owInviteOk' href='https://health-transformation.optimalworkshop.com/questions/j0373s8u?tag=intercept&utm_medium=intercept' target='_blank' style='color:#FFFFFF;font-size:16px;font-weight:bold;text-decoration:underline;'>Take a survey</a><a id='owInviteCancel' href='javascript:void(0)' onclick='this.parentNode.style.display="none";' style='color:#FAF5F5;font-size:14px;text-decoration:underline;float:right;'>No thanks</a></div><script>var owOnload=function(){if(-1==document.cookie.indexOf('owInvite')){var o=new XMLHttpRequest;o.onloadend=function(){try{var o=document.getElementById('owInviteSnippet');this.response&&JSON.parse(this.response).active===!0&&(document.cookie='owInvite=Done',setTimeout(function(){o.style.display='block',o.style.opacity=1},2e3))}catch(e){}},o.open('POST','https://www.optimalworkshop.com/survey_status/questions/xj228804/active'),o.send()}};if(window.addEventListener){window.addEventListener('load',function(){owOnload();});}else if(window.attachEvent){window.attachEvent('onload',function(){owOnload();});}</script>
+  <!-- END Optimal Workshop snippet -->
 </body>
 </html>
