81db00fa4a440d310bf061adf4c10099e9f8b47e
Josh Li <oasislrjjrl@gmail.com>
2018-01-22 16:33:32 +1100

    [develop] Added proper field structure to publication collection field template.

--- health/templates/health_templates/publication_collection.tpl.php
+++ health/templates/health_templates/publication_collection.tpl.php
@@ -1,4 +1,8 @@
-<div class=publication-collection">
-  <b>Part of a collection:</b>
-  <?php print $collection_list; ?>
+<div class="field-name-field-publication-collection field-label-above">
+    <div class="field-label">
+        Part of a collection:
+    </div>
+    <div class="field-items">
+      <?php print $collection_list; ?>
+    </div>
 </div>
