6a920b8df469f251c8cb03c6664c57036fe5822e
Danny <dannymarlow78@hotmail.com>
2018-02-12 15:25:44 +1100

    GP/HP switch template update. Go to downloads text update. Filter style mobile fixes.

--- health/templates/health_templates/public_hp_switcher.tpl.php
+++ health/templates/health_templates/public_hp_switcher.tpl.php
@@ -1,13 +1,12 @@
 <div class="block block-bean contextual-links-region first last odd block-bean-call-to-action rs_skip" id="block-bean-professionals-hub-additional-inf">
   <a href="<?php print $url; ?>">
     <?php if ($title): ?>
-      <h3 class="block__title"><?php print $title; ?></h3>
+      <h3 class="block__title uikit-cta-link"><?php print $title; ?></h3>
     <?php endif; ?>
       <div class="entity entity-bean bean-call-to-action clearfix" typeof="">
           <div class="bean-block-content">
             <?php if($text): ?>
                 <p><?php print $text; ?></p>
-                <p class="uikit-cta-link"><?php print $link_text; ?></p>
             <?php endif; ?>
           </div>
       </div>
