a7bfe28b428d47c627ff6c8b36a541f45f7723bd
Danny <dannymarlow78@hotmail.com>
2018-02-13 15:24:57 +1100

    HP/GP

--- health/templates/health_templates/public_hp_switcher.tpl.php
+++ health/templates/health_templates/public_hp_switcher.tpl.php
@@ -1,12 +1,13 @@
 <div class="block block-bean block-bean-call-to-action">
   <?php if ($title): ?>
-  <h3 class="block__title uikit-cta-link">
+  <p class="bean-call-to-action__title">
+    <i class="fas fa-angle-right forward-icon"></i>
     <a href="<?php print $url; ?>">
       <?php print $title; ?>
-    </a>
-  </h3>
+    </a> 
+</p>
   <?php endif; ?>
-  <div class="entity entity-bean bean-call-to-action clearfix" typeof="">
+  <div class="entity entity-bean clearfix" typeof="">
     <div class="bean-block-content">
       <?php if($text): ?>
       <p>
