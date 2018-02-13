172ce79d4c9b412cf3032b65af7902ab603e335b
Josh Li <oasislrjjrl@gmail.com>
2018-02-13 14:46:06 +1100

    Added full to section link div if there is no content in title_supp region.

--- health/templates/health_templates/section_link.tpl.php
+++ health/templates/health_templates/section_link.tpl.php
@@ -1,6 +1,4 @@
-<div class="section-header rs_skip">
-  <i class="fas fa-angle-left back-icon"></i>
-  <a href="<?php print url($path)?>">
+<i class="fas fa-angle-left back-icon"></i>
+<a href="<?php print url($path)?>">
     <span><?php print $title?></span>
-  </a>
-</div>
+</a>
