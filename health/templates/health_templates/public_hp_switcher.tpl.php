5f746ca9e507230b41e6ac4d6bbff1dc89e93cff
Danny <dannymarlow78@hotmail.com>
2018-02-13 13:29:55 +1100

    HP/GP switcher changes

--- health/templates/health_templates/public_hp_switcher.tpl.php
+++ health/templates/health_templates/public_hp_switcher.tpl.php
@@ -1,14 +1,18 @@
-<div class="block block-bean contextual-links-region first last odd block-bean-call-to-action rs_skip" id="block-bean-professionals-hub-additional-inf">
-  <a href="<?php print $url; ?>">
-    <?php if ($title): ?>
-      <h3 class="block__title uikit-cta-link"><?php print $title; ?></h3>
-    <?php endif; ?>
-      <div class="entity entity-bean bean-call-to-action clearfix" typeof="">
-          <div class="bean-block-content">
-            <?php if($text): ?>
-                <p><?php print $text; ?></p>
-            <?php endif; ?>
-          </div>
-      </div>
-  </a>
+<div class="block block-bean block-bean-call-to-action">
+  <?php if ($title): ?>
+  <h3 class="block__title uikit-cta-link">
+    <a href="<?php print $url; ?>">
+      <?php print $title; ?>
+    </a>
+  </h3>
+  <?php endif; ?>
+  <div class="entity entity-bean bean-call-to-action clearfix" typeof="">
+    <div class="bean-block-content">
+      <?php if($text): ?>
+      <p>
+        <?php print $text; ?>
+      </p>
+      <?php endif; ?>
+    </div>
+  </div>
 </div>
