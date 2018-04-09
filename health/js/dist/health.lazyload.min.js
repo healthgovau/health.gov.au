34abbe348674a5bd552001bf2167fe3760eec160
Dylan Rolfe <dylan.rolfe@health.gov.au>
2018-04-09 13:43:06 +1000

    [HT-782] Logging on images disappearing when read speaker plays.

--- health/js/dist/health.lazyload.min.js
+++ health/js/dist/health.lazyload.min.js
@@ -5,8 +5,10 @@
   Drupal.behaviors.lazyLoadImages = {
     attach: function (context, settings) {
       if (typeof LazyLoad !== 'undefined') {
+        console.log('lazyload');
         var myLazyLoad = new LazyLoad({
           callback_load: function (el) {
+            console.log('callback_load');
             // Remove all the space reserving class and styles.
             $(el).parents('.image-wrapper')
               .removeClass('image-loading')
