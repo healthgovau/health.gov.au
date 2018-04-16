dc8766b7cdb1a1d7a64da49710de5c45f6d88097
Dylan Rolfe <dylan.rolfe@health.gov.au>
2018-04-16 11:40:28 +1000

    [18171] - Updated the ‘In this section’ local nav toggle styling

--- health/templates/block--menu-block--2.tpl.php
+++ health/templates/block--menu-block--2.tpl.php
@@ -7,21 +7,13 @@
  * @see https://drupal.org/node/1728246
  */
 ?>
-<div class="block" id="block-bean-local-navigation-menu-toggle-for">
-    <div class="entity entity-bean bean-basic-content local-navigation-menu-toggle-for clearfix" about="/block/local-navigation-menu-toggle-for" typeof="">
-        <div class="field field-name-field-bean-body field-type-text-long field-label-hidden">
-            <div class="field-items">
-                <div class="field-item even">
-                    <div class="mobile-toggle mobile-toggle__local-nav health-accordion">
-                        <a href="#" class="mobile-toggle__open toc-filter-processed health-accordion__title health_accordion--closed"  aria-controls="block-menu-block-2">
-                          <span>In this section</span>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+
+<div class="health-accordion">
+  <a href="#" class="localnav__mobile-toggle health-accordion__title health_accordion--closed" aria-controls="block-menu-block-2">
+    <span>In this section</span>
+  </a>
 </div>
+
 <div class="<?php print $classes; ?> health-accordion health-accordion__body health-accordion--closed"<?php print $attributes; ?> id="<?php print $block_html_id; ?>">
 
   <?php if ($block->region != 'sidebar_second') : ?>
