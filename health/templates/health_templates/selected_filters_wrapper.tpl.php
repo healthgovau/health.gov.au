36e5ee48066b6e95e2dc7ee88bf2a2b5b12f6d8b
Dylan Rolfe <dylan.rolfe@health.gov.au>
2018-04-24 15:11:14 +1000

    [18097] More updates for UIKit v2

--- health/templates/health_templates/selected_filters_wrapper.tpl.php
+++ health/templates/health_templates/selected_filters_wrapper.tpl.php
@@ -1,4 +1,4 @@
-<div id="edit-custom-remove" class="form-item form-type-item">
+<div id="edit-custom-remove" class="form-item form-item--inline form-type-item">
   <?php if ($selected_filters): ?>
     <label for="edit-custom-remove">Filters applied:</label>
     <div class="facet-remove">
