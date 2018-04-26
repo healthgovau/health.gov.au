9f479d087eb8920fc932c2f0356d3fe166a01974
Dylan Rolfe <dylan.rolfe@health.gov.au>
2018-04-26 16:35:40 +1000

    [18097] More updates for UIKit v2

--- health/templates/health_templates/public_hp_switcher.tpl.php
+++ health/templates/health_templates/public_hp_switcher.tpl.php
@@ -1,8 +1,6 @@
 <div class="block block-bean block-bean-call-to-action">
   <?php if ($title): ?>
-    <a href="<?php print $url; ?>">
-      <?php print $title; ?>
-    </a> 
+    <a href="<?php print $url; ?>"><?php print $title; ?></a>
     <i class="fas fa-angle-right forward-icon"></i>
   <?php endif; ?>
   <div class="entity entity-bean clearfix" typeof="">
