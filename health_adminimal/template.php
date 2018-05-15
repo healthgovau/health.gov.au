2238ca5c17a23b1ffb345ad45c20187331d2dec7
Dylan Rolfe <dylan.rolfe@health.gov.au>
2018-05-15 10:24:53 +1000

    [19442] Turn single checkbox into a group of checkboxes so that it gets an outer label in line with our other fields.

--- health_adminimal/template.php
+++ health_adminimal/template.php
@@ -268,6 +268,14 @@ function health_adminimal_field_widget_form_alter(&$element, &$form_state, $cont
     $element['add_more']['add_more']['#value'] = t('Add');
     $element['add_more']['type']['#prefix'] = '<svg aria-hidden="true" data-prefix="fas" data-icon="plus-circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-plus-circle fa-w-16 fa-3x"><path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm144 276c0 6.6-5.4 12-12 12h-92v92c0 6.6-5.4 12-12 12h-56c-6.6 0-12-5.4-12-12v-92h-92c-6.6 0-12-5.4-12-12v-56c0-6.6 5.4-12 12-12h92v-92c0-6.6 5.4-12 12-12h56c6.6 0 12 5.4 12 12v92h92c6.6 0 12 5.4 12 12v56z" class=""></path></svg>';
   }
+
+  // Change single checkbox boolean into a checkbox group so that it gets a label.
+  if (isset($element['#type']) && $element['#type'] == 'checkbox') {
+    $element['#type'] = 'checkboxes';
+    $element['#options'] = [$element['#on_value'] => $element['#title']];
+    $element['#default_value'] = [$element['#default_value']];
+    $element['#title'] = $form_state['field'][$element['#field_name']][LANGUAGE_NONE]['instance']['label'];
+  }
 }
 
 /**
