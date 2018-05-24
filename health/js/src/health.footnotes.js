5ee9bd20c6f6e99976973df52ae23c9eea384434
Dylan Rolfe <dylan.rolfe@health.gov.au>
2018-05-24 14:51:35 +1000

    [19416] - Moved glossary to a paragraph rather than a content type. Used dark theme for tippy.

--- health/js/src/health.footnotes.js
+++ health/js/src/health.footnotes.js
@@ -1,23 +1,24 @@
-(function ($) {
-  Drupal.health = Drupal.health || {};
+(function ($, Drupal, window, document) {
+
+  'use strict';
 
   /**
    * Add a highlight class when a footnote or reference link is clicked.
    */
-  Drupal.health.footnotes = function() {
-    $('.reference__link a, .footnote__link a')
-      .each(function() {
-        $(this).attr('title', $($(this).attr('href')).parent('li,p').text());
-      })
-      .click(function(e) {
-        e.preventDefault();
-        $(e.target.hash).parent('li,p').addClass('highlight');
-        window.setTimeout(function() {
-          $(e.target.hash).parent('li,p').removeClass('highlight');
-        }, 2000);
-      });
+  Drupal.behaviors.footnotes = {
+    attach: function (context, settings) {
+      $('.reference__link a, .footnote__link a')
+        .each(function() {
+          $(this).attr('title', $($(this).attr('href')).parent('li,p').text());
+        })
+        .click(function(e) {
+          e.preventDefault();
+          $(e.target.hash).parent('li,p').addClass('highlight');
+          window.setTimeout(function() {
+            $(e.target.hash).parent('li,p').removeClass('highlight');
+          }, 2000);
+        });
+    }
   };
 
-  Drupal.health.footnotes();
-  
-})(jQuery);
+})(jQuery, Drupal, this, this.document);
