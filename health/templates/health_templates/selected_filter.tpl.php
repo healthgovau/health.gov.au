24b0e8a6e3ec8572c8b7cb269ec1d44c56846fdb
Josh Li <oasislrjjrl@gmail.com>
2018-06-04 13:02:24 +1000

    [19416-long-docs] Fixed format in selected filters.

--- health/templates/health_templates/selected_filter.tpl.php
+++ health/templates/health_templates/selected_filter.tpl.php
@@ -1,4 +1,4 @@
 <a href="<?php print $url; ?>" class="<?php print $classes; ?>">
-  <?php print $text; ?>
+  <?php print ucfirst(str_replace('_', ' ', $text)); ?>
   <i class="fa fa-times-circle" aria-hidden="true"></i>
 </a>
