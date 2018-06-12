d81c73bac3344ff309bfb6ac061da7b31da1485c
Dylan Rolfe <dylan.rolfe@health.gov.au>
2018-06-12 16:47:15 +1000

    [develop] - Added some missing aria roles (alert, navigation). Added aria-label to recommendation callout.

--- health/template.php
+++ health/template.php
@@ -1060,3 +1060,36 @@ function health_query_node_access_alter(QueryAlterableInterface $query) {
     unset($c[0]);
   }
 }
+
+/**
+ * Implements theme_status_messages().
+ *
+ * Add role="alert".
+ */
+function health_status_messages($variables) {
+  $display = $variables['display'];
+  $output = '';
+  $status_heading = array(
+    'status' => t('Status message'),
+    'error' => t('Error message'),
+    'warning' => t('Warning message'),
+  );
+  foreach (drupal_get_messages($display) as $type => $messages) {
+    $output .= "<div role=\"alert\" class=\"messages {$type}\">\n";
+    if (!empty($status_heading[$type])) {
+      $output .= '<h2 class="element-invisible">' . $status_heading[$type] . "</h2>\n";
+    }
+    if (count($messages) > 1) {
+      $output .= " <ul>\n";
+      foreach ($messages as $message) {
+        $output .= '  <li>' . $message . "</li>\n";
+      }
+      $output .= " </ul>\n";
+    }
+    else {
+      $output .= reset($messages);
+    }
+    $output .= "</div>\n";
+  }
+  return $output;
+}
