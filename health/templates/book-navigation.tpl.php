20209e605f789b9dee8f7171f377938df0dce15f
Dylan Rolfe <dylan.rolfe@health.gov.au>
2018-06-12 16:48:54 +1000

    [19416] - Updated book navigation (prev, next) to use the uikit direction links.

--- health/templates/book-navigation.tpl.php
+++ health/templates/book-navigation.tpl.php
@@ -33,27 +33,24 @@
  */
 ?>
 <?php if ($has_links): ?>
-  <div id="book-navigation-<?php print $book_id; ?>" class="book-navigation">
-    <?php if ($has_links): ?>
-    <div class="book-navigation__links clearfix">
+  <div id="book-navigation-<?php print $book_id; ?>" class="book-navigation" role="navigation" aria-label="Book">
+    <div class="row">
       <?php if ($prev_url): ?>
-        <div class="book-navigation__link-wrapper book-navigation__link--previous">
-          <a href="<?php print $prev_url; ?>" class="book-navigation__link" title="<?php print t('Go to previous page'); ?>">
-            <i aria-hidden="true" class="fa fa-angle-left"></i>Previous
+        <div class="book-navigation__link-wrapper col-sm-6">
+          <a class="uikit-direction-link uikit-direction-link--left" href="<?php print $prev_url; ?>">
+            Previous
           </a>
           <span class="book-navigation__link-title"><?php print $prev_title; ?></span>
         </div>
       <?php endif; ?>
       <?php if ($next_url): ?>
-      <div class="book-navigation__link-wrapper book-navigation__link--next">
-        <a href="<?php print $next_url; ?>" class="book-navigation__link" title="<?php print t('Go to next page'); ?>">
-          Next<i aria-hidden="true" class="fa fa-angle-right"></i>
+      <div class="book-navigation__link-wrapper book-navigation__link-wrapper--right col-sm-6">
+        <a class="uikit-direction-link uikit-direction-link--right" href="<?php print $next_url; ?>">
+          Next
         </a>
         <span class="book-navigation__link-title"><?php print $next_title; ?></span>
       </div>
       <?php endif; ?>
     </div>
-    <?php endif; ?>
-
   </div>
 <?php endif; ?>
