47b232d944f1a81829849214cf242e3958073fd1
Dylan Rolfe <dylan.rolfe@health.gov.au>
2018-06-18 14:42:56 +1000

    [18097] - Updated breadcrumbs in line with uikit update.

--- health/templates/health_templates/health_breadcrumb.tpl.php
+++ health/templates/health_templates/health_breadcrumb.tpl.php
@@ -1,6 +1,6 @@
-<nav class="breadcrumbs rs_skip" role="navigation">
+<nav class="au-breadcrumbs rs_skip" aria-label="Breadcrumb">
   <h2 class="element-invisible"><?php print $title ?></h2>
-  <ol class="breadcrumbs__list">
+  <ol class="au-link-list au-link-list--inline">
     <?php foreach ($breadcrumb as $crumb) : ?>
       <li><?php print $crumb ?></li>
     <?php endforeach; ?>
