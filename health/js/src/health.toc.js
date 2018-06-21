c59f89a6a803ab3aeb4069b2fe01466adb1a840e
Dylan Rolfe <dylan.rolfe@health.gov.au>
2018-06-21 11:50:53 +1000

    [19416] - Remove duplicate code

--- health/js/src/health.toc.js
+++ health/js/src/health.toc.js
@@ -37,29 +37,6 @@
         navigation: '.toc',
         headers: 'h2',
       });
-
-      // Generate id's for all headings that don't have them so we can deep link.
-      $('h2,h3,h4,h5,h6').each(function () {
-        if ($(this).attr('id') === undefined) {
-          var id = $(this).text().replace(/[^\w\s]/gi, '')
-            .replace(/\s+/g, '-')
-            .toLowerCase();
-          $(this).attr('id', id);
-        }
-      });
-      // Once it has finished adding anchors, check the url anchor fragment and scroll to that heading if needed.
-      if (window.location.hash) {
-        // Find hash target.
-        var $a = $(window.location.hash);
-        // Make hash target is on the current page.
-        if (!$a.length) {
-          return true;
-        }
-        // Scroll to hash target
-        $('html, body').animate({scrollTop: $a.offset().top}, 'medium');
-        $a.focus();
-      }
-
     }
   };
 
