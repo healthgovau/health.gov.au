6b86093780c69400c8e0eb5b889d1009d571bf40
Dylan Rolfe <dylan.rolfe@health.gov.au>
2018-06-28 12:30:37 +1000

    [19416] - Improved accessibility of footnote, reference, abbrevation and definition
    Removed the links surrounding these elements as it didn't meet WCAG 2.4.4

--- health/js/src/health.definitions.js
+++ health/js/src/health.definitions.js
@@ -5,7 +5,7 @@
       if (settings.health.definitions) {
         $('span.definition').each(function() {
           // Check the parent, we don't want to replace in all situations.
-          if ($(this).parent().not('h1,h2,h3,h4,h5,h6,a').length > 0) {
+          if ($(this).parent().not('h1,h2,h3,h4,h5,h6,a,span.reference__link,span.footnote__link').length > 0) {
             // Find this term in the list and create a replacement for it.
             for (var i=0; i<settings.health.definitions.terms.length; i++) {
               var term = settings.health.definitions.terms[i];
--- health/js/src/health.footnotes.js
+++ health/js/src/health.footnotes.js
@@ -8,22 +8,11 @@
   Drupal.behaviors.footnotes = {
     attach: function (context, settings) {
       // Find all the footnote and reference links.
-      $('.reference__link a, .footnote__link a')
+      $('.reference__link, .footnote__link')
         .each(function() {
           // Add a title attribute to each link based on the full footnote/reference.
           // This is used for tippy js.
           $(this).attr('title', $($(this).attr('aria-describedby')).html());
-        })
-        .click(function(e) {
-          // Don't scroll down if we are on mobile.
-          if ($(window).width() < 768) {
-            e.preventDefault();
-          }
-          // Add a highlight class.
-          $('a[name="' + e.target.hash.substring(1) + '"]').parent('li').addClass('highlight');
-          window.setTimeout(function () {
-            $('a[name="' + e.target.hash.substring(1) + '"]').parent('li').removeClass('highlight');
-          }, 2000);
         });
     }
   };
