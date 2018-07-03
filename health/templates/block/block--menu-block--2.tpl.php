ac1753da63df4e817ab7dae3b66c2af6f7bb2839
Dylan Rolfe <dylan.rolfe@health.gov.au>
2018-07-03 11:47:58 +1000

    TX-140 Local navigation refactor

--- health/templates/block/block--menu-block--2.tpl.php
+++ health/templates/block/block--menu-block--2.tpl.php
@@ -8,21 +8,12 @@
  */
 ?>
 
-<div class="health-accordion">
-  <a href="#" class="localnav__mobile-toggle health-accordion__title health-accordion--closed" aria-controls="block-menu-block-2">
+<div class="au-local-navigation__mobile-toggle health-accordion">
+  <a href="#" class="health-accordion__title health-accordion--closed" aria-controls="block-menu-block-2">
     <span>In this section</span>
   </a>
 </div>
 
 <div class="<?php print $classes; ?> health-accordion health-accordion__body"<?php print $attributes; ?> id="<?php print $block_html_id; ?>">
-
-  <?php if ($block->region != 'sidebar_second') : ?>
-    <?php print render($title_prefix); ?>
-    <?php if ($title): ?>
-      <h3<?php print $title_attributes; ?>><?php print $title; ?></h3>
-    <?php endif; ?>
-    <?php print render($title_suffix); ?>
-  <?php endif; ?>
-
   <?php print $content; ?>
 </div>
