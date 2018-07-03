3afb8a1454695b4558479f7bc8b0ff0879a748a7
Dylan Rolfe <dylan.rolfe@health.gov.au>
2018-07-03 16:23:41 +1000

    TX-60 Updated 'On this page'

--- health/templates/health_templates/toc.tpl.php
+++ health/templates/health_templates/toc.tpl.php
@@ -1,3 +1,7 @@
-<nav class="au-inpage-nav-links rs_preserve" aria-label="On this page">
-  <div class="au-inpage-nav-links__heading"></div>
-</nav>
+<div class="row">
+  <div class="col-xs-12">
+    <nav class="au-inpage-nav-links rs_preserve" aria-label="On this page">
+      <div class="au-inpage-nav-links__heading"></div>
+    </nav>
+  </div>
+</div>
