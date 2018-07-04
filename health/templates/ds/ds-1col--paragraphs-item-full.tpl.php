47b62c66f7ad981297d93a717d7de9bcef5308fd
Dylan Rolfe <dylan.rolfe@health.gov.au>
2018-07-04 10:35:17 +1000

    TX-60 Swapped row and container as they were in the wrong order

--- health/templates/ds/ds-1col--paragraphs-item-full.tpl.php
+++ health/templates/ds/ds-1col--paragraphs-item-full.tpl.php
@@ -5,8 +5,8 @@
  * Display Suite 1 column template.
  */
 ?>
-<div class="row para-row <?php print $classes; ?>">
-  <<?php print $ds_content_wrapper; print $layout_attributes; ?> class="container ds-1col clearfix">
+<div class="container para-row <?php print $classes; ?>">
+  <<?php print $ds_content_wrapper; print $layout_attributes; ?> class="row ds-1col clearfix">
     <?php if (isset($title_suffix['contextual_links'])): ?>
       <?php print render($title_suffix['contextual_links']); ?>
     <?php endif; ?>
