e261f6bf828a957c400484b14fc0fa8e33731cc7
Dylan Rolfe <dylan.rolfe@health.gov.au>
2018-07-09 15:49:14 +1000

    TX-141 - Added missing row in health layout ds templates

--- health/ds_layouts/health_2col/health-2col.tpl.php
+++ health/ds_layouts/health_2col/health-2col.tpl.php
@@ -5,26 +5,28 @@
  * Display Suite 2 column template.
  */
 ?>
-<div class="container">
-  <div class="row">
-    <div <?php print $layout_attributes; ?> class="<?php print $classes;?> clearfix">
+<div class="row">
+  <div class="container">
+    <div class="row">
+      <div <?php print $layout_attributes; ?> class="<?php print $classes;?> clearfix">
 
-      <?php if (isset($title_suffix['contextual_links'])): ?>
-        <?php print render($title_suffix['contextual_links']); ?>
+        <?php if (isset($title_suffix['contextual_links'])): ?>
+          <?php print render($title_suffix['contextual_links']); ?>
+        <?php endif; ?>
+
+        <div class="<?php print $left_classes; ?>">
+          <?php print $left; ?>
+        </div>
+
+        <div class="<?php print $right_classes; ?>">
+          <?php print $right; ?>
+        </div>
+
+      </div>
+
+      <?php if (!empty($drupal_render_children)): ?>
+        <?php print $drupal_render_children ?>
       <?php endif; ?>
-
-      <div class="<?php print $left_classes; ?>">
-        <?php print $left; ?>
-      </div>
-
-      <div class="<?php print $right_classes; ?>">
-        <?php print $right; ?>
-      </div>
-
     </div>
-
-    <?php if (!empty($drupal_render_children)): ?>
-      <?php print $drupal_render_children ?>
-    <?php endif; ?>
   </div>
 </div>
--- health/ds_layouts/health_2col_stacked/health-2col-stacked.tpl.php
+++ health/ds_layouts/health_2col_stacked/health-2col-stacked.tpl.php
@@ -5,34 +5,36 @@
  * Display Suite 2 column template.
  */
 ?>
-<div class="container">
-  <div class="row">
-    <div <?php print $layout_attributes; ?> class="<?php print $classes;?> clearfix">
+<div class="row">
+  <div class="container">
+    <div class="row">
+      <div <?php print $layout_attributes; ?> class="<?php print $classes;?> clearfix">
 
-      <?php if (isset($title_suffix['contextual_links'])): ?>
-        <?php print render($title_suffix['contextual_links']); ?>
+        <?php if (isset($title_suffix['contextual_links'])): ?>
+          <?php print render($title_suffix['contextual_links']); ?>
+        <?php endif; ?>
+
+        <div class="<?php print $top_classes; ?>">
+          <?php print $top; ?>
+        </div>
+
+        <div class="<?php print $left_classes; ?>">
+          <?php print $left; ?>
+        </div>
+
+        <div class="<?php print $right_classes; ?>">
+          <?php print $right; ?>
+        </div>
+
+        <div class="<?php print $bottom_classes; ?>">
+          <?php print $bottom; ?>
+        </div>
+
+      </div>
+
+      <?php if (!empty($drupal_render_children)): ?>
+        <?php print $drupal_render_children ?>
       <?php endif; ?>
-
-      <div class="<?php print $top_classes; ?>">
-        <?php print $top; ?>
-      </div>
-
-      <div class="<?php print $left_classes; ?>">
-        <?php print $left; ?>
-      </div>
-
-      <div class="<?php print $right_classes; ?>">
-        <?php print $right; ?>
-      </div>
-
-      <div class="<?php print $bottom_classes; ?>">
-        <?php print $bottom; ?>
-      </div>
-
     </div>
-
-    <?php if (!empty($drupal_render_children)): ?>
-      <?php print $drupal_render_children ?>
-    <?php endif; ?>
   </div>
 </div>
