09f81f2792e3998bdbb940d23c95743ce2041ef3
Dylan Rolfe <dylan.rolfe@health.gov.au>
2018-07-10 16:59:10 +1000

    [18097-uikit-v2] Added missing row on 1 column template

--- health/ds_layouts/health_1col/health-1col.tpl.php
+++ health/ds_layouts/health_1col/health-1col.tpl.php
@@ -5,22 +5,24 @@
  * Display Suite 2 column template.
  */
 ?>
-<div class="container">
-  <div class="row">
-    <div <?php print $layout_attributes; ?> class="<?php print $classes;?> clearfix">
+<div class="row">
+  <div class="container">
+    <div class="row">
+      <div <?php print $layout_attributes; ?> class="<?php print $classes;?> clearfix">
 
-      <?php if (isset($title_suffix['contextual_links'])): ?>
-        <?php print render($title_suffix['contextual_links']); ?>
-      <?php endif; ?>
+        <?php if (isset($title_suffix['contextual_links'])): ?>
+          <?php print render($title_suffix['contextual_links']); ?>
+        <?php endif; ?>
 
-      <div class="<?php print $middle_classes; ?>">
-        <?php print $middle; ?>
+        <div class="<?php print $middle_classes; ?>">
+          <?php print $middle; ?>
+        </div>
+
       </div>
 
+      <?php if (!empty($drupal_render_children)): ?>
+        <?php print $drupal_render_children ?>
+      <?php endif; ?>
     </div>
-
-    <?php if (!empty($drupal_render_children)): ?>
-      <?php print $drupal_render_children ?>
-    <?php endif; ?>
   </div>
 </div>
