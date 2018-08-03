3ecd845163c0a0ecb1a070ad7eeb962f86059ca0
Josh Li <oasislrjjrl@gmail.com>
2018-08-03 12:27:59 +1000

    Removed 'search results' wording from result listing.

--- health/templates/search_api/search-api-page-results.tpl.php
+++ health/templates/search_api/search-api-page-results.tpl.php
@@ -41,7 +41,6 @@
   <?php endif; ?>
   <?php print render($spellcheck); ?>
   <?php if ($result_count): ?>
-    <h2><?php print t('Search results');?></h2>
     <?php print render($search_results); ?>
     <?php print render($pager); ?>
   <?php else : ?>
