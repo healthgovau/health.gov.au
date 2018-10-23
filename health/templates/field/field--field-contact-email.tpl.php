a8d40239b490a22753fcb538b870c7417483be32
Dylan Rolfe <mr.dylan.rolfe@gmail.com>
2018-10-23 15:15:37 +1100

    TX-408 Add classes now that field--inline is gone

--- health/templates/field/field--field-contact-email.tpl.php
+++ health/templates/field/field--field-contact-email.tpl.php
@@ -46,7 +46,7 @@
  * @ingroup themeable
  */
 ?>
-<div class="<?php print $classes; ?>"<?php print $attributes; ?>>
+<div class="col-sm-9 <?php print $classes; ?>"<?php print $attributes; ?>>
   <?php if (!$label_hidden): ?>
     <div class="field-label"<?php print $title_attributes; ?>><?php print $label ?>:&nbsp;</div>
   <?php endif; ?>
--- health/templates/field/field--field-contact-telephone.tpl.php
+++ health/templates/field/field--field-contact-telephone.tpl.php
@@ -46,6 +46,8 @@
  * @ingroup themeable
  */
 ?>
+
+<?php if ($element['#view_mode'] != 'hotline_bar') { $classes.= ' col-sm-9'; } ?>
 <div class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php if (!$label_hidden): ?>
     <div class="field-label"<?php print $title_attributes; ?>><?php print $label ?>:&nbsp;</div>
