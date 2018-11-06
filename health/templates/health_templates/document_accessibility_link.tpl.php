88bff687be0e7e63ec6ccb46b49873702a91c586
Dylan Rolfe <mr.dylan.rolfe@gmail.com>
2018-11-06 14:40:39 +1100

    TX-519 Update the accessibility statement on resources

--- health/templates/health_templates/document_accessibility_link.tpl.php
+++ health/templates/health_templates/document_accessibility_link.tpl.php
@@ -1,3 +1,8 @@
 <div class="document-accessibility-form">
-  <p>These files may not be suitable for users of assistive technology. <a href="/request-accessible-format?referrer=<?php print $current_page; ?>" title="Request an accessible format">Request an accessible format</a>.</p>
+  <p>We aim to provide documents in an accessible format. If you're having
+    problems using a document with your accessibility tools,
+    <a href="/request-accessible-format?referrer=<?php print $current_page; ?>"
+       title="Request an accessible format">please contact us for help</a>.
+  </p>
 </div>
+
