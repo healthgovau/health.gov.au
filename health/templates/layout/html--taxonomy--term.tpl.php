6afd8395a66fd80bf1e077a1fe64aa94552a9876
Dylan Rolfe <mr.dylan.rolfe@gmail.com>
2018-11-13 11:58:13 +1100

    TX-529 Show 404 instead of taxonomy term page

---
+++ health/templates/layout/html--taxonomy--term.tpl.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @file
+ * Returns the HTML for the basic html structure of a single Drupal page.
+ *
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728208
+ */
+?>
+
